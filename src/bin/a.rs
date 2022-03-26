60a5b8dd3c7756c5fd803d60eeaf45b5e696c7be
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2022-03-26 15:20:16 +0900

    check

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -20,7 +20,80 @@ use std::time::SystemTime;
 #[allow(dead_code)]
 const SIDE: usize = 20;
 
-struct Input {}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Coord {
+    x: isize,
+    y: isize,
+}
+
+#[allow(dead_code)]
+impl Coord {
+    fn new(p: (isize, isize)) -> Self {
+        Coord { x: p.0, y: p.1 }
+    }
+    fn from_usize_pair(p: (usize, usize)) -> Self {
+        Coord {
+            x: p.0 as isize,
+            y: p.1 as isize,
+        }
+    }
+
+    fn in_field(&self) -> bool {
+        (0 <= self.x && self.x < SIDE as isize) && (0 <= self.y && self.y < SIDE as isize)
+    }
+
+    // ペアへの変換
+    fn to_pair(&self) -> (isize, isize) {
+        (self.x, self.y)
+    }
+
+    // マンハッタン距離
+    fn distance(&self, that: &Self) -> isize {
+        (self.x - that.x).abs() + (self.y - that.y).abs()
+    }
+
+    fn mk_4dir(&self) -> Vec<Self> {
+        let delta = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+        delta
+            .iter()
+            .map(|&p| self.plus(&Coord::new(p)))
+            .filter(|&pos| pos.in_field())
+            .collect()
+    }
+
+    fn com_to_delta(com: char) -> Self {
+        match com {
+            'U' => Coord::new((0, -1)),
+            'D' => Coord::new((0, 1)),
+            'L' => Coord::new((-1, 0)),
+            'R' => Coord::new((1, 0)),
+            _ => unreachable!(),
+        }
+    }
+
+    // 四則演算
+    fn plus(&self, that: &Self) -> Self {
+        Coord::new((self.x + that.x, self.y + that.y))
+    }
+    fn minus(&self, that: &Self) -> Self {
+        Coord::new((self.x - that.x, self.y - that.y))
+    }
+
+    fn access_matrix<'a, T>(&'a self, mat: &'a Vec<Vec<T>>) -> &'a T {
+        &mat[self.y as usize][self.x as usize]
+    }
+
+    fn set_matrix<T>(&self, mat: &mut Vec<Vec<T>>, e: T) {
+        mat[self.y as usize][self.x as usize] = e;
+    }
+}
+
+struct Input {
+    start: Coord,
+    goal: Coord,
+    p: f64,
+}
 impl Input {}
 
 #[fastout]
@@ -33,12 +106,20 @@ fn main() {
         sj: usize,
         gi: usize,
         gj: usize,
+
         p: f64,
+
         h: [Chars; SIDE],
         v: [Chars; SIDE - 1],
     }
 
-    let ans = "";
+    let sp = Coord::from_usize_pair((sj, si));
+    let gp = Coord::from_usize_pair((gj, gi));
+
+    let mut ans = "".to_string();
+    for _ in 0..50 {
+        ans = format!("{}{}", ans, "UDLR");
+    }
 
     println!("{}", ans);
 
